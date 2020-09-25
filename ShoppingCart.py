216f0565294d6d75c1dcd4a63d1918c590a9c394
Tej <tjsahu@gmail.com>
2020-09-25 22:18:35 +0530

    Shopping Cart

---
+++ ShoppingCart.py
@@ -0,0 +1,25 @@
+# Split example
+string = "Hello, welcome to my world"
+# print(string.split(" "))
+
+# Shopping receipts program
+p1_item, p1_price = "Balls", 4.95
+p2_item, p2_price = "Pens", 2.50
+p3_item, p3_price = "Monitor", 450.00
+
+# create a company name and address
+company_name = "coding temple inc."
+company_address = "283 Franklin St."
+company_city = "Boston, MA"
+
+# end message
+message = "Thank you for shopping with us today"
+
+print('*' * 50)
+print("\t\t{}".format(company_name.title()))
+print("\t\t{}".format(company_address))
+print("\t\t{}".format(company_city))
+
+print('=' * 50)
+
+print("\t\t{}".format(p1_item, p1_price))
